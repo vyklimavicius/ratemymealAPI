ee46d55f24e80f7830ee1d1ea4d9901f9249cf40
vyklimavicius <vyklimavicius@gmail.com>
2019-11-20 16:35:07 -0500

    minor DB fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Rating.create(value: 1)
+Rating.create(value: 2)
+Rating.create(value: 3)
+Rating.create(value: 4)
+Rating.create(value: 5)
